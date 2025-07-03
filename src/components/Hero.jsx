3ad4d950e3bd7d44d902950d4d31a6b21c228ad7
vstoj <vstojadinovic.996@gmail.com>
2025-07-03 19:16:13 +0200

    hero changed subtitle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -69,7 +69,7 @@ const Hero = () => {
   return (
     <>
       <section id="hero" className="noisy">
-        <h1 className="title">Plaza Bar</h1>
+        <h1 className="title">Cocktail Bar</h1>
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
